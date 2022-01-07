4fc21acb7ab154f4015bb4ed745f75cba826d05a
Spencer Hulse <hulse.spencer@gmail.com>
2022-01-07 18:35:06 -0500

    added logic to the form to handle username searches

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+
 let getUserRepos = (user) => {
   //format the github api url
   let apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -9,5 +12,21 @@ let getUserRepos = (user) => {
     });
   });
 };
+
+let formSubmitHandler = (event) => {
+  event.preventDefault();
+
+  //get value from input element
+  let username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
 
 getUserRepos();
